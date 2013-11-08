d70386677c9dd95c1d083da76a3b56b50bf0be26
ajfaraday <andrew@AJF-work-lap.(none)>
2013-11-08 19:24:22 +0000

    initial sources model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+
+# 
+# RSS sources
 #
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+Source.create!(:name => "BBC News", :url => "http://feeds.bbci.co.uk/news/rss.xml")
+Source.create!(:name => 'Daily Mail', :url => "http://www.dailymail.co.uk/home/index.rss")
+Source.create!(:name => "CNN", :url => "http://rss.cnn.com/rss/edition.rss")
+Source.create!(:name => 'FOX News', :url => "http://feeds.foxnews.com/foxnews/latest")
+
+
