4287425f11f72d3c07b0ab02a972108ef4a00043
ajfaraday <andrew@AJF-work-lap.(none)>
2013-11-08 19:05:59 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsMoreFun::Application.config.secret_token = '21895fe4ed4f0797b83e86e36d51d44a50519d8b18fc7bf04b578a4d1d5b99af8a10f2aae5b222ad23e0f4be2ce3e022b282cd08bd0a1cfd93eddf5f370984d2'
